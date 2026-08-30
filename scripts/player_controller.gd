88fdd58c8e23cf6002e8c20f9bae46dce3949d8c
Ethan Warren <27879801@sun.ac.za>
2026-08-30 02:03:09 +0200

    added rudamentarty player controller

---
+++ scripts/player_controller.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+enum State {IDLE, RUN, ATTACK}
+@onready var _player_sprite = $AnimatedSprite2D
+var _state: State = State.IDLE
+
+func _ready() -> void:
+	_player_sprite.animation_finished.connect(_on_animation_finished)
+
+func _physics_process(_delta: float) -> void:
+	if _state == State.ATTACK:
+		velocity = Vector2.ZERO
+	else:
+		var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		velocity = dir * SPEED
+
+		if Input.is_action_just_pressed("attack"):
+			_set_state(State.ATTACK)
+		elif velocity.length() > 0.0:
+			_set_state(State.RUN)
+		else:
+			_set_state(State.IDLE)
+
+	if velocity.x != 0.0:
+		_player_sprite.flip_h = velocity.x < 0.0
+
+	move_and_slide()
+
+func _set_state(new_state: State) -> void:
+	if new_state == _state:
+		return
+	_state = new_state
+	match _state:
+		State.IDLE:
+			_player_sprite.play("idle")
+		State.RUN:
+			_player_sprite.play("run")
+		State.ATTACK:
+			_player_sprite.play("attack")
+
+func _on_animation_finished() -> void:
+	if _state == State.ATTACK:
+		_set_state(State.IDLE)
