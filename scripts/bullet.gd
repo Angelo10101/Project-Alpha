292d879e0769d6928336a3cf599aa5c4300f6920
fbr <zoobmarshall@gmail.com>
2026-09-02 10:53:27 +0200

    added shooting + other mechanics

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,11 +1,22 @@
 extends Area2D
 
 @export var speed: float = 400.0
+@export var damage: float = 10.0
+@export var target: String = "mob"
+
 var direction: Vector2 = Vector2.RIGHT
 
 func _ready() -> void:
+	body_entered.connect(_hitbox_tag)
 	await get_tree().create_timer(2.0).timeout
 	queue_free()
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
+
+func _hitbox_tag(body: Node2D) -> void:
+	if not body.is_in_group(target):
+		return
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	queue_free()
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+const MAX_HEALTH = 100.0
+
 enum State {IDLE, RUN, ATTACK}
+
+@export var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
+
 @onready var _player_sprite = $AnimatedSprite2D
+
 var _state: State = State.IDLE
+var health: float = MAX_HEALTH
 
 func _ready() -> void:
 	_player_sprite.animation_finished.connect(_on_animation_finished)
@@ -17,6 +23,7 @@ func _physics_process(_delta: float) -> void:
 		velocity = dir * SPEED
 
 		if Input.is_action_just_pressed("attack"):
+			_shoot()
 			_set_state(State.ATTACK)
 		elif velocity.length() > 0.0:
 			_set_state(State.RUN)
@@ -27,6 +34,25 @@ func _physics_process(_delta: float) -> void:
 		_player_sprite.flip_h = velocity.x < 0.0
 
 	move_and_slide()
+	
+func _shoot() -> void:
+	if bullet_scene == null:
+		return
+	var bullet = bullet_scene.instantiate()
+	
+	bullet.global_position = global_position
+	bullet.direction = (get_global_mouse_position() - global_position).normalized()
+	bullet.target = "mob"
+	get_tree().current_scene.add_child(bullet)
+	
+func _take_damage(amount: float) -> void:
+	health -= amount
+	if health <= 0.0:
+		_death()
+		
+func _death() -> void:
+	# TODO: game has ended screen or just button that restarts for testing 
+	queue_free()
 
 func _set_state(new_state: State) -> void:
 	if new_state == _state:
